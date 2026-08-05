c697384fa44244c6e6293074c7921cd692e6a043
untodesu <kirill@untode.su>
2026-08-05 17:51:02 +0500

    CMakePresets.json

--- scripts/pack-generic-release.sh
+++ scripts/pack-generic-release.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd "$(dirname $(dirname "${0}"))" || exit 1
-${SHELL} scripts/build-generic-release.sh || exit 1
-cpack -G ${1:-TGZ} --config build/generic-release/CPackConfig.cmake || exit 1
+cmake --preset generic-release || exit 1
+cmake --build --preset generic-release || exit 1
+cpack --preset generic-release || exit 1
 exit 0
